05afa984577213ad0b4ba3f031314d6843ca4d7f
nolan <nolan.zhang@yunzhihui.com>
2015-01-19 10:32:20 +0800

    删除无效文件

--- src/main/java/com/cloudwise/smartagent/resource/CollectResource.java
+++
@@ -1,9 +0,0 @@
-package com.cloudwise.smartagent.resource;
-
-public class CollectResource {
-	private static final String ENDPOINT = "http://portal.toushibao.com/";
-
-	public void send(Object obj) {
-		
-	}
-}
